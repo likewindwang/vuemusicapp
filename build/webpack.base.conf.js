97bc6b51c6dedf1da6996ebc6164c87dc4074eb5
webyan <737809475@qq.com>
2017-10-24 13:38:50 +0800

    头部组件完成

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,8 +22,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+
+      'src': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components')
     }
   },
   module: {
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
